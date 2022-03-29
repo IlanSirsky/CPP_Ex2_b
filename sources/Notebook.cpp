a21456f3f8695c683e5da30514e2c4f3df66ba05
Ilan <ilan1il1000@gmail.com>
2022-03-29 17:47:29 +0300

    Done

--- sources/Notebook.cpp
+++ sources/Notebook.cpp
@@ -3,42 +3,74 @@
 using namespace std;
 using namespace ariel;
 
-char Notebook::read_map(int x, int y)
-{
-    auto ptr = notebook.find(pair<int, int>(x, y));
-    if (ptr == notebook.end())
-    {
-        return '_';
+const int MAX_COL = 99;
+const int SHOW_ROWS = 100;
+const int mincharASCII = 32;
+const int maxCharASCII = 126;
+
+void Notebook::check_input(int page, int row, int column, Direction direction, int length){
+    if((direction == Direction::Horizontal && column + length > MAX_COL + 1) 
+        || (column < 0 || page < 0 || row < 0 || length < 0)
+        || column > MAX_COL){
+        throw invalid_argument("Invalid input");
     }
-    return ptr->second;
 }
 
 void Notebook::write(int page, int row, int column, Direction direction, string const &str)
 {
-    if (page < 0 || row < 0 || column < 0){
-        throw "Invalid input";
-    }
+    check_input(page, row, column, direction, str.length());
+
     for (int i = 0; i < str.length(); i++)
     {
-        if (str[i] == '~'){
-            throw("Invalid input");
+        if(str.at((size_t) i) < mincharASCII || str.at((size_t) i) > maxCharASCII){
+            throw invalid_argument("Invalid string input");
+        }
+        if (str.at((size_t) i) == '~'){
+            throw invalid_argument("Invalid string input");
         }
     }
+    
     switch (direction)
     {
         case Direction::Horizontal:
         {
+            if (notebook[pair<int, int>(page, row)].empty())
+            {
+                for (int i = 0; i < MAX_COL + 1; i++)
+                {
+                    notebook[pair<int, int>(page, row)].push_back('_');
+                } 
+            }
             for (int i = 0; i < str.length(); i++)
             {
-                notebook[pair<int, int>(column + i, row)] = str.at(i);
+                int pos = column + i;
+                if (notebook[pair<int, int>(page, row)].at((size_t) pos) != '_')
+                {
+                    throw invalid_argument("Trying to overwrite an existing char");
+                }
             }
+            notebook[pair<int, int>(page, row)].replace((size_t) column, str.length(), str);
         }
         break;
+
         case Direction::Vertical:
         {
+            for (int i = 0; i < str.length(); i++) {
+                if (notebook[pair<int, int>(page, row + i)].empty()) {
+                    for (int j = 0; j < MAX_COL + 1; j++) {
+                        notebook[pair<int, int>(page, row + i)].push_back('_');
+                    }
+                }
+                if (notebook[pair<int, int>(page, row + i)].at((size_t) column) != '_')
+                {
+                    throw invalid_argument("Trying to overwrite an existing char");
+                }
+            }
             for (int i = 0; i < str.length(); i++)
             {
-                notebook[pair<int, int>(column, row + i)] = str.at(i);
+                string s;
+                s.push_back(str.at((size_t) i));
+                notebook[pair<int, int>(page, row + i)].replace((size_t) column, 1, s);
             }
         }
         break;
@@ -47,26 +79,40 @@ void Notebook::write(int page, int row, int column, Direction direction, string 
 
 string Notebook::read(int page, int row, int column, Direction direction, int length)
 {
-    if (page < 0 || row < 0 || column < 0){
-        throw "Invalid input";
-    }
+    check_input(page, row, column, direction, length);
+
     string ans;
     switch (direction)
     {
         case Direction::Horizontal:
         {
-            for (u_int i = 0; i < length; i++)
+            if (!notebook[pair<int, int>(page, row)].empty())
             {
-                ans.push_back(read_map(column + i, row));
+                for (int i = 0; i < length; i++){
+                    ans.push_back(notebook[pair<int, int>(page, row)].at((size_t) column + (size_t) i));
+                }
+                return ans;
             }
+
+            for (int i = 0; i < length; i++)
+            {
+                ans.push_back('_');
+            }
+            return ans;
+
         }
         break;
+
         case Direction::Vertical:
         {
-            for (u_int i = 0; i < length; i++)
-            {
-                ans.push_back(read_map(column, row + i));
+            for (int i = 0; i < length; ++i) {
+            if (notebook[pair<int, int>(page, row + i)].empty()) {
+                ans.push_back('_');
+            } else {
+                ans.push_back(notebook[pair<int, int>(page, row + i)].at((size_t) column ));
             }
+        }
+            return ans;
         }
         break;
     }
@@ -75,47 +121,48 @@ string Notebook::read(int page, int row, int column, Direction direction, int le
 
 void Notebook::erase(int page, int row, int column, Direction direction, int length)
 {
-    if (page < 0 || row < 0 || column < 0){
-        throw "invalid input";
+    check_input(page, row, column, direction, length);
+    
+    switch (direction)
+    {
+        case Direction::Horizontal:
+        {
+            if (notebook[pair<int, int>(page, row)].empty())
+            {
+                for (int i = 0; i < MAX_COL + 1; i++)
+                {
+                    notebook[pair<int, int>(page, row)].push_back('_');
+                } 
+            }
+            string del;
+            for (int i = 0; i < length; i++) {
+                del.push_back('~');
+            }
+            notebook[pair<int, int>(page, row)].replace((size_t) column, (size_t) length, del);
+        }
+        break;
+
+        case Direction::Vertical:
+        {
+            for (int i = 0; i < length; i++) {
+                if (notebook[pair<int, int>(page, row + i)].empty()) {
+                    for (int j = 0; j < MAX_COL + 1; j++) {
+                        notebook[pair<int, int>(page, row + i)].push_back('_');
+                    }
+                }
+                notebook[pair<int, int>(page, row + i)].replace((size_t) column, 1, "~");
+            }
+        }
+        break;
     }
 }
 
 void Notebook::show(int page)
 {
     if (page < 0){
-        throw "invalid input";
+        throw invalid_argument("Invalid page number");
     }
-    int end_x = 0;
-    int end_y = 0;
-    int start_x = INT_MAX; 
-    int start_y = INT_MAX;
-    // std::map<pair<u_int, u_int>, char>::iterator iter = board.begin();
-    for(auto element : notebook) // element is of type: pair<pair<u_int, u_int>, char>
-    {
-        if (element.first.first < start_x)
-        {
-            start_x = element.first.first;
-        }
-        if (element.first.first > end_x)
-        {
-            end_x = element.first.first;
-        }
-        if (element.first.second < start_y)
-        {
-            start_y = element.first.second;
-        }
-        if (element.first.second > end_y)
-        {
-            end_y = element.first.second;
-        }
-    }
-
-    for (u_int y = start_y; y <= end_y; y++)
-    {
-        for (u_int x = start_x; x <= end_x; x++)
-        {
-            cout << read_map(x, y) << ends;
-        }
-        cout << endl;
+    for (int i = 0; i < SHOW_ROWS; i++) {
+        //cout << read(page, i, 0, Direction::Horizontal, MAX_COL) << endl;
     }
 }
--- sources/Notebook.hpp
+++ sources/Notebook.hpp
@@ -4,19 +4,25 @@
 #include <iostream>
 #include <string>
 #include <climits>
-#include <tuple>
 #include <map>
 #include <stdexcept>
 using namespace std;
 
 namespace ariel {
     class Notebook{
-            std::map<pair<int, int>, char> notebook;
+
+        map<pair<int, int>, string> notebook;
+
         public:
-        char read_map(u_int x, u_int y);
+        
+        static void check_input(int page, int row, int column, Direction direction, int length);
         void write(int page, int row, int column, Direction direction, string const &str);
-        static string read(int page, int row, int column, Direction direction, int length);
+        string read(int page, int row, int column, Direction direction, int length);
         void erase(int page, int row, int column, Direction direction, int length);
-        void show(int page);
+        static void show(int page);
     };
 }
+
+
+
+
